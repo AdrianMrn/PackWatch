b852e9b44d81584d7038410d084f528fb2baa816
Adriaan Marain <adriaanmarain300@gmail.com>
2017-11-27 15:42:02 +0100

    add items and read with phone

--- arduino/PackWatch/PackWatch.ino
+++ arduino/PackWatch/PackWatch.ino
@@ -1,43 +1,81 @@
-// send a NDEF message to adnroid or get a NDEF message
-//
-// note: [NDEF library](https://github.com/Don/NDEF) is needed.
+#include <SPI.h>
+#include "PN532_SPI.h"
+#include "PN532.h"
+#include "NfcAdapter.h"
 
 #include "SPI.h"
-#include "PN532_SPI.h"
 #include "snep.h"
 #include "NdefMessage.h"
 
-PN532_SPI pn532spi(SPI, 10);
-SNEP nfc(pn532spi);
-uint8_t ndefBuf[128];
+PN532_SPI interface(SPI, 10); // create a SPI interface for the shield with the SPI CS terminal at digital pin 10
 
-void setup()
+
+String listOfItems;
+String newItem;
+
+void setup(void)
 {
-    Serial.begin(115200);
-    Serial.println("-------Peer to Peer--------");
+    Serial.begin(115200); // start serial comm
+    Serial.println("NDEF Reader");
 }
 
-void loop()
+void loop(void)
 {
+    NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
+    nfc.begin(); // begin NFC comm
+      
+    Serial.println("\nScan an NFC tag\n");
+    if (nfc.tagPresent()) // Do an NFC scan to see if an NFC tag is present
+    {
+        NfcTag tag = nfc.read(); // read the NFC tag
+        if(tag.hasNdefMessage())
+        {
+          NdefMessage message = tag.getNdefMessage();
+          for(int i=0;i<message.getRecordCount();i++)
+          {
+            NdefRecord record = message.getRecord(i);
+            int payloadLength = record.getPayloadLength();
+            byte payload[payloadLength];
+            record.getPayload(payload);
+  
+            String strPayload = String((char*)payload);
+  
+            newItem = strPayload[3];
+            newItem += strPayload[4];
+  
+            if (listOfItems.indexOf(newItem) == -1) {
+              listOfItems += newItem;
+            }
+  
+            Serial.println(listOfItems);
+          }
+        } else {
+          p2p();
+        }
+    }
+    delay(250);
+}
+
+void p2p(void)
+{
+    SNEP nfc(interface);
+    uint8_t ndefBuf[128];
+
     Serial.println("Send a message to Android");
     NdefMessage message = NdefMessage();
-    message.addUriRecord("A1B2C3D4F5");
+    message.addUriRecord(listOfItems);
     int messageSize = message.getEncodedSize();
     if (messageSize > sizeof(ndefBuf)) {
         Serial.println("ndefBuf is too small");
         while (1) {
         }
-
     }
 
     message.encode(ndefBuf);
     if (0 >= nfc.write(ndefBuf, messageSize)) {
-        Serial.println("wtf");
         Serial.println("Failed :(");
     } else {
         Serial.println("Success");
     }
-
-    delay(250);
 }
 
