3e0dffa30c06b452280d68f1e6865153ac335650
Adriaan Marain <adriaanmarain300@gmail.com>
2017-12-27 14:37:09 +0100

    laravel passport with test auth in nodejs

--- backend/laravel/routes/web.php
+++ backend/laravel/routes/web.php
@@ -16,16 +16,18 @@ Route::get('/', function () {
     /* return ("kek"); */
 });
 
-Route::get('/kek', function () {
-    return "kek";
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('item', 'ItemController');
-Route::resource('pack', 'PackController');
-Route::resource('linkitemspacks', 'LinkitemspacksController');
-Route::resource('schedule', 'ScheduleController');
-Route::resource('user', 'UserController');
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::resource('item', 'ItemController');
+    Route::resource('pack', 'PackController');
+    Route::resource('linkitemspacks', 'LinkitemspacksController');
+    Route::resource('schedule', 'ScheduleController');
+    Route::resource('user', 'UserController');
+
+    Route::get('/kek', function () {
+        return "authentication successful!";
+    });
+});
