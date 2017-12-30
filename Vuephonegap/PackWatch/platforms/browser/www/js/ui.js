3304a6c7c43892d466ef72e5ff1175b88a7c8157
Dieter Vercammen <dietervercammen93@gmail.com>
2017-12-30 17:09:55 +0100

    vue and phongap project

---
+++ Vuephonegap/PackWatch/platforms/browser/www/js/ui.js
@@ -0,0 +1,53 @@
+
+new Vue({
+    el: '#app',
+    data: {
+      
+        e2: 0,
+        startNFC: true,
+        stopNFC: false,
+        e1: 'recent',
+        dialog:false,
+        stepper1: 0,
+        section1: true, 
+        section2: false,
+    },
+    computed: {
+      computedColor () {
+        switch (this.e2) {
+          case 0:
+            return 'blue-grey'
+          break
+          case 1:
+            return 'teal'
+          break
+          case 2:
+            return 'brown'
+          break
+          case 3:
+            return 'lime darken-3'
+          break
+        }
+      },
+    }
+  });
+
+Vue.component('pack-alert', {
+    
+
+    
+    template: ` 
+    <div>
+            <v-card>
+            <v-card-title class="headline text-lg-center">Info</v-card-title>
+            <v-card-text>Please hold the NFC tag near the phoooone!</v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" flat @click="$emit('close-alert')">The fuck?</v-btn>
+                <v-btn color="green darken-1" flat >OK</v-btn>
+            </v-card-actions>
+            </v-card>
+    </div>
+    `,
+
+})
